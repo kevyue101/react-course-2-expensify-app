9795dc5e2441172d4defa1e849c546d514f51ac9
Kevin Yue <kevin.yue@iress.com.au>
2021-03-07 23:17:36 +1100

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -13,33 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-
-
-const waterBill = store.dispatch(addExpense({
-    description: 'Water bill',
-    amount: 10000,
-    createdAt: moment().toString()
-    })
-);
-
-const gasBill = store.dispatch(addExpense({
-    description: 'Gas bill',
-    amount: 1800,
-    createdAt: moment().toString()
-    })
-);
-
-const rent = store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 5000000,
-    createdAt: moment().toString()
-    })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
